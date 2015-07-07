031f4f89b42d955538d6efee131891e02599f10f
George Wu <georgewu_mac@GeorgematoMacBook-Air.local>
2015-07-07 22:13:44 +0800

    Initial commit.

---
+++ TestProjectMay/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TestProjectMay
+//
+//  Created by George Wu on 7/7/15.
+//  Copyright © 2015 George Wu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
